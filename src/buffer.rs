a905edfee55de2b6db3254ee1a0fd01513638348
Crea7dosSantos <cr26yusaku0720@icloud.com>
2021-08-23 23:32:04 +0900

    define buffer struct

---
+++ src/buffer.rs
@@ -0,0 +1,30 @@
+use std::cell::{Cell, RefCell};
+use std::collections::HashMap;
+use std::io;
+use std::ops::{Index, IndexMut};
+use std::rc::Rc;
+
+use crate::disk::{DiskManager, PageId, PAGE_SIZE};
+
+pub type Page = [u8; PAGE_SIZE];
+pub struct Buffer {
+    pub page_id: PageId,
+    pub page: RefCell<Page>,
+    pub is_dirty: Cell<bool>,
+}
+
+pub struct Frame {
+    usage_count: u64,
+    buffer: Rc<Buffer>,
+}
+
+pub struct BufferPool {
+    buffers: Vec<Frame>,
+    next_victim_id: BufferId,
+}
+
+pub struct BufferPoolManager {
+    disk: DiskManager,
+    pool: BufferPool,
+    page_table: HashMap<PageId, BufferId>,
+}
