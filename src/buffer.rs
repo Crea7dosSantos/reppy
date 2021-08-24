9c0c9b7e77601368f69288e0ffc202c365b553e6
Crea7dosSantos <cr26yusaku0720@icloud.com>
2021-08-24 23:17:21 +0900

    close #8

--- src/buffer.rs
+++ src/buffer.rs
@@ -23,6 +23,35 @@ pub struct BufferPool {
     next_victim_id: BufferId,
 }
 
+impl BufferPool {
+    fn evict(&mut self) -> Option<BufferId> {
+        let pool_size = self.size();
+        let mut consecutive_pinned = 0;
+        let victim_id = loop {
+            let next_victim_id = self.next_victim_id;
+            let frame = &mut self[next_victim_id];
+            if frame.usage_count == 0 {
+                break self.next_victim_id;
+            }
+            if Rc::get_mut(&mut frame.buffer).is_some() {
+                frame.usage_count -= 1;
+                consecutive_pinned = 0;
+            } else {
+                consecutive_pinned += 1;
+                if consecutive_pinned >= pool_size {
+                    return None;
+                }
+            }
+            self.next_victim_id = self.increment_id(self.next_victim_id);
+        };
+        Some(victim_id)
+    }
+
+    fn increment_id(&self, buffer_id: BufferId) -> BufferId {
+        BufferId((buffer_id.0 + 1) % self.size())
+    }
+}
+
 pub struct BufferPoolManager {
     disk: DiskManager,
     pool: BufferPool,
